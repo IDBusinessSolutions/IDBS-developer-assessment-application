d858df74750b2c6b2a3a94bc2ef3b162c754ecc0
rnaylorIDBS <32928390+rnaylorIDBS@users.noreply.github.com>
2020-01-08 17:37:04 +0000

    updated to use referenceLibs folder

--- java_option/run_IDBSDeveloperAssessment.cmd
+++ java_option/run_IDBSDeveloperAssessment.cmd
@@ -2,5 +2,5 @@
 # It uses the default IDBS Example solution as the basis of the question and solution for the application
 # It assumes that the cmd file is located in the same directory/folder as the IDBSDeveloperAssessment.jar
 
-java -cp ".\IDBSDeveloperAssessment.jar;" com.idbs.devassessment.core.IDBSDeveloperAssessmentMain
+java -cp ".\referenceLibs\IDBSDeveloperAssessment.jar;" com.idbs.devassessment.core.IDBSDeveloperAssessmentMain
 
