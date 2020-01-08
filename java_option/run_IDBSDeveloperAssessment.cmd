c7012ff82fce5d2ad209dfcbe6d6bd902294d917
rnaylorIDBS <32928390+rnaylorIDBS@users.noreply.github.com>
2020-01-08 16:46:22 +0000

    app start dos cmd file

---
+++ java_option/run_IDBSDeveloperAssessment.cmd
@@ -0,0 +1,6 @@
+# This is the execution command line to use to run the IDBS Developer Assessment application using the jar files supplied from github.
+# It uses the default IDBS Example solution as the basis of the question and solution for the application
+# It assumes that the cmd file is located in the same directory/folder as the IDBSDeveloperAssessment.jar
+
+java -cp ".\IDBSDeveloperAssessment.jar;" com.idbs.devassessment.core.IDBSDeveloperAssessmentMain
+
