e1478b538aa6602a1274b637c97cbc79f05ca0a1
rnaylorIDBS <32928390+rnaylorIDBS@users.noreply.github.com>
2020-01-09 18:47:39 +0000

    fixed missing json jars

--- java_option/run_IDBSDeveloperAssessment.cmd
+++ java_option/run_IDBSDeveloperAssessment.cmd
@@ -2,5 +2,4 @@
 # It uses the default IDBS Example solution as the basis of the question and solution for the application
 # It assumes that the cmd file is located in the same directory/folder as the IDBSDeveloperAssessment.jar
 
-java -cp ".\referenceLibs\IDBSDeveloperAssessment.jar;" com.idbs.devassessment.core.IDBSDeveloperAssessmentMain
-
+java -cp ".\referenceLibs\IDBSDeveloperAssessment.jar;.\referenceLibs\javax.json-1.0.4.jar;.\referenceLibs\javax.json-api-1.1.jar" com.idbs.devassessment.core.IDBSDeveloperAssessmentMain
