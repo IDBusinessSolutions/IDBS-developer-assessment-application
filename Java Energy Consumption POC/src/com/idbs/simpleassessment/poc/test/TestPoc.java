7d34d446812bfad047d1da4942e785c5dd05dfeb
rnaylorIDBS <32928390+rnaylorIDBS@users.noreply.github.com>
2021-10-11 12:02:19 +0100

    Add files via upload

--- Java Energy Consumption POC/src/com/idbs/simpleassessment/poc/test/TestPoc.java
+++ Java Energy Consumption POC/src/com/idbs/simpleassessment/poc/test/TestPoc.java
@@ -26,21 +26,21 @@ public class TestPoc
 
             double test01EnergyUsed = poc.calculateLocationEnergyConsumption(LONDON_WATERLOO_FEED_ID);
             String test01Str = new DecimalFormat("#0.00").format(test01EnergyUsed / 1000);
-            System.out.println("Energy Used at Waterloo Office is: " + test01Str + " Kw     (expected: 9.57 Kw)");
+            System.out.println("Calculated energy used at Waterloo Office is: " + test01Str + " Kw     (expected: 9.57 Kw)");
 
             double test02EnergyUsed = poc.calculateLocationEnergyConsumption(GUILDFORD_FEED_ID);
             String test02Str = new DecimalFormat("#0.00").format(test02EnergyUsed / 1000);
-            System.out.println("Energy Used at Guildford Office is: " + test02Str + " Kw     (expected: 209.31 Kw)");
+            System.out.println("Calculated energy used at Guildford Office is: " + test02Str + " Kw     (expected: 209.31 Kw)");
 
             double test03EnergyUsed = poc.calculateLocationEnergyConsumption(PORTLAND_MAINE_FEED_ID);
             String test03Str = new DecimalFormat("#0.00").format(test03EnergyUsed / 1000);
             System.out.println(
-                "Energy Used at Portland Maine Office is: " + test03Str + " Kw     (expected: 387.13 Kw)");
+                "Calculated energy used at Portland Maine Office is: " + test03Str + " Kw     (expected: 387.13 Kw)");
 
             double test04EnergyUsed = poc.calculateLocationEnergyConsumption(ALAMEDA_CALIFORNIA_FEED_ID);
             String test04Str = new DecimalFormat("#0.00").format(test04EnergyUsed / 1000);
             System.out.println(
-                "Energy Used at Alemeda California Office is: " + test04Str + " Kw     (expected: 4651.63 Kw)");
+                "Calculated energy used at Alemeda California Office is: " + test04Str + " Kw     (expected: 4651.63 Kw)");
 
             return (test01Str.equals("9.57") && test02Str.equals("209.31") && test03Str.equals("387.13")
                 && test04Str.equals("4651.63"));
