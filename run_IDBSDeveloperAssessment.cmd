489bfe9f573951d7e59f587daedbc170e096eff0
rnaylorIDBS <32928390+rnaylorIDBS@users.noreply.github.com>
2022-05-17 13:56:25 +0100

    Delete run_IDBSDeveloperAssessment.cmd

--- run_IDBSDeveloperAssessment.cmd
+++
@@ -1,5 +0,0 @@
-REM This is the execution command line to use to run the IDBS Developer Assessment application using the jar files supplied from github.
-REM It uses the default IDBS Example solution as the basis of the question and solution for the application
-REM It assumes that the cmd file is located in the same directory/folder as the IDBSDeveloperAssessment.jar
-
-java -cp ".\ReferenceLibs\IDBSDeveloperAssessment.jar;.\ReferenceLibs\javax.json-1.0.4.jar;.\ReferenceLibs\javax.json-api-1.1.jar" com.idbs.devassessment.core.IDBSDeveloperAssessmentMain
