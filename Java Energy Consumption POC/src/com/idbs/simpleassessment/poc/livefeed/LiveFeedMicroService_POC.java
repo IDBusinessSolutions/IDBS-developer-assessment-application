19a79a01e1ae09f04977b64c5a5f7884aa4e5639
rnaylorIDBS <32928390+rnaylorIDBS@users.noreply.github.com>
2022-09-05 10:44:24 +0100

    Add files via upload

--- Java Energy Consumption POC/src/com/idbs/simpleassessment/poc/livefeed/LiveFeedMicroService_POC.java
+++ Java Energy Consumption POC/src/com/idbs/simpleassessment/poc/livefeed/LiveFeedMicroService_POC.java
@@ -4,8 +4,10 @@ import static com.idbs.simpleassessment.poc.TransformEnergyDataToStandardDataSto
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -18,41 +20,39 @@ import java.util.Map;
  * simple impl for the POC only - will be replace with reading data from a live feed in production, probably a
  * micro-service.
  * 
- * DO NOT REFACTOR
+ * DO NOT REFACTOR THIS CLASS
+ * 
+ * IT IS HERE TO MOCK A LIVE FEED OF DATA
  *
- */
+ **/
 public class LiveFeedMicroService_POC
 {
+    private final String feedId;
+
+    public LiveFeedMicroService_POC(String feedId)
+    {
+        this.feedId = feedId;
+    }
+
     //
     // DO NOT REFACTOR THIS METHOD
     // IT IS HERE TO MOCK A LIVE FEED OF DATA
     //
-    static public String readDataFromLiveFeed(String feedId) throws IOException
+    public String readData() throws IOException
     {
         String retVal = null;
         File file = null;
 
-        if (feedId.equals(LONDON_WATERLOO_FEED_ID))
-        {
-            file = new File(LONDON_WATERLOO_FEED_ID + ".dat");
-        }
-        else if (feedId.equals(GUILDFORD_FEED_ID))
-        {
-            file = new File(GUILDFORD_FEED_ID + ".dat");
-        }
-        else if (feedId.equals(PORTLAND_MAINE_FEED_ID))
-        {
-            file = new File(PORTLAND_MAINE_FEED_ID + ".dat");
-        }
-        else if (feedId.equals(ALAMEDA_CALIFORNIA_FEED_ID))
-        {
-            file = new File(ALAMEDA_CALIFORNIA_FEED_ID + ".dat");
-        }
-        else
-        {
-            throw new IOException("unknown data feed identifier - canot continue: " + feedId);
-        }
+        file = determineSourceDataFile();
 
+        retVal = readFeedData(file);
+
+        return retVal;
+    }
+
+    private String readFeedData(File file) throws IOException, FileNotFoundException
+    {
+        String retVal;
         // now read the data from file...
         try (BufferedReader reader = new BufferedReader(new FileReader(file)))
         {
@@ -64,17 +64,45 @@ public class LiveFeedMicroService_POC
                 if (!line.startsWith("#"))
                 {
                     stringBuilder.append(line);
-                    if (!feedId.equals(ALAMEDA_CALIFORNIA_FEED_ID))
-                    {
-                        stringBuilder.append(" // ");
-                    }
                 }
             }
 
-            stringBuilder.setLength(stringBuilder.length() - 4);
-
             retVal = stringBuilder.toString();
         }
+        return retVal;
+    }
+
+    private File determineSourceDataFile() throws IOException
+    {
+        File retVal = null;
+
+        if (feedId.equals(LONDON_WATERLOO_FEED_ID))
+        {
+            retVal = new File(LONDON_WATERLOO_FEED_ID + ".dat");
+        }
+        else if (feedId.equals(GUILDFORD_FEED_ID))
+        {
+            retVal = new File(GUILDFORD_FEED_ID + ".dat");
+        }
+        else if (feedId.equals(PORTLAND_MAINE_FEED_ID))
+        {
+            retVal = new File(PORTLAND_MAINE_FEED_ID + ".dat");
+        }
+        else if (feedId.equals(ALAMEDA_CALIFORNIA_FEED_ID))
+        {
+            retVal = new File(ALAMEDA_CALIFORNIA_FEED_ID + ".dat");
+        }
+        else
+        {
+            throw new IOException("unknown data feed identifier - canot continue: " + feedId);
+        }
+
+        return retVal;
+    }
+
+    public static LiveFeedMicroService_POC getFeedFor(String feedId)
+    {
+        LiveFeedMicroService_POC retVal = new LiveFeedMicroService_POC(feedId);
 
         return retVal;
     }
